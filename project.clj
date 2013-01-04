862bae600a4f511127b4077e3434c953a46b5c7a
Mark Roghelia <mark@roboshark.com>
2013-01-04 11:37:21 -0500

    Add project URL.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-copy-deps "0.1.0"
   :description "Copies all the jar dependencies to a directory"
+  :url "https://github.com/mjroghelia/lein-copy-deps"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
