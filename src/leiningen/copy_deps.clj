f965215228355d0a5ba337c48f852ada7b79ffdb
Mark Roghelia <mark@roboshark.com>
2013-01-04 11:06:58 -0500

    Remove debugging output.

--- src/leiningen/copy_deps.clj
+++ src/leiningen/copy_deps.clj
@@ -1,12 +1,10 @@
 (ns leiningen.copy-deps
-    (:require [clojure.pprint :as pp]
-              [clojure.java.io :as io]
+    (:require [clojure.java.io :as io]
               [leiningen.core.classpath :as cp]))
 
 (defn copy-deps [project]
   (let [dir (io/file "target" "lib")
         jars (map io/file (filter (partial re-find #"\.jar$") (cp/get-classpath project)))]
-    (pp/pprint project)
     (if-not (.exists dir)
       (.mkdir dir))
     (doseq [jar jars]
